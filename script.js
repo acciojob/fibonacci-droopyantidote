215279f40ca42de16112c79a0ff1879587f72993
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-13 10:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        let prevPrev = 0;
+        let prev = 1;
+        let current = 0;
+        for (let i = 2; i <= num; i++) {
+            current = prev + prevPrev;
+            prevPrev = prev;
+            prev = current;
+        }
+        return prevPrev;
+    }
 }
 
-module.exports = fibonacci;
+// Test cases
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
+
