4599ae94c00abd14a8280a7d3d82f95cbfdc1a1e
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-13 10:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 function fibonacci(num) {
-let num1 = 0;
-    let num2 = 1;
-    let sum;
-    let i = 0;
-    for (i = 0; i < num; i++) {
-        sum = num1 + num2;
-        num1 = num2;
-        num2 = sum;
-    }
-    return num2;  
+if (num == 1)
+        return 0;
+    if (num == 2)
+        return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);  
 }
 
 
