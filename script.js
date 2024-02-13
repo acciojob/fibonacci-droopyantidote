5ed23dfe45a805710997a2eb402f7dc4fd3901e2
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-13 10:35:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ function fibonacci(num) {
     }
 }
 
-// Test cases
-console.log(fibonacci(1)); // Output: 0
-console.log(fibonacci(5)); // Output: 3
+
 
 module.exports = fibonacci
 
-
+// Test cases
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
